5f7f4d7b313c31d683bc63793a5e81b395f2067d
Geperson Mamalias <kuyagepofficial@gmail.com>
2023-09-17 19:39:45 +0800

    updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,8 +23,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <link rel="stylesheet" href="{{ asset('assets/') }}/dist/css/adminlte.min.css">
 </head>
 
-<body class="hold-transition layout-top-nav dark-mode">
-    <div class="wrapper">
+<body class="hold-transition layout-top-nav ">
+    <div class="wrapper ">
 
         <!-- Navbar -->
         <nav class="main-header navbar navbar-expand-md navbar-dark navbar-dark accent-secondary">
@@ -188,65 +188,42 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <div class="content">
                 <div class="container">
                     <div class="row">
-                        <div class="col-lg-6">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-
-                                    <p class="card-text">
-                                        Some quick example text to build on the card title and make up the bulk of the
-                                        card's
-                                        content.
-                                    </p>
-
-                                    <a href="#" class="card-link">Card link</a>
-                                    <a href="#" class="card-link">Another link</a>
+                        <div class="col-lg-12">
+                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active">
+                                    </li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img src="{{ asset('brand_logo/hero-1.png') }}" class="d-block w-100"
+                                            alt="hero banner">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="{{ asset('brand_logo/hero-2.png') }}" class="d-block w-100"
+                                            alt="hero banner">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="https://www.deped.gov.ph/wp-content/uploads/2023/01/BER-banner-1536x569.jpg"
+                                            class="d-block w-100" alt="...">
+                                    </div>
                                 </div>
-                            </div>
-
-                            <div class="card card-primary card-outline">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-
-                                    <p class="card-text">
-                                        Some quick example text to build on the card title and make up the bulk of the
-                                        card's
-                                        content.
-                                    </p>
-                                    <a href="#" class="card-link">Card link</a>
-                                    <a href="#" class="card-link">Another link</a>
-                                </div>
-                            </div><!-- /.card -->
-                        </div>
-                        <!-- /.col-md-6 -->
-                        <div class="col-lg-6">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h5 class="card-title m-0">Featured</h5>
-                                </div>
-                                <div class="card-body">
-                                    <h6 class="card-title">Special title treatment</h6>
-
-                                    <p class="card-text">With supporting text below as a natural lead-in to additional
-                                        content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-
-                            <div class="card card-primary card-outline">
-                                <div class="card-header">
-                                    <h5 class="card-title m-0">Featured</h5>
-                                </div>
-                                <div class="card-body">
-                                    <h6 class="card-title">Special title treatment</h6>
-
-                                    <p class="card-text">With supporting text below as a natural lead-in to additional
-                                        content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
+                                <button class="carousel-control-prev" type="button"
+                                    data-target="#carouselExampleIndicators" data-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button"
+                                    data-target="#carouselExampleIndicators" data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </button>
                             </div>
                         </div>
                         <!-- /.col-md-6 -->
+
                     </div>
                     <!-- /.row -->
                 </div><!-- /.container-fluid -->
@@ -265,10 +242,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <footer class="main-footer">
             <!-- To the right -->
             <div class="float-right d-none d-sm-inline">
-                Anything you want
+                Developed with ❤️ by Geperson C. Mamalias
             </div>
             <!-- Default to the left -->
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
+            <strong>Copyright &copy; {{ date('Y') }} <a
+                    href="{{ url('/') }}">{{ config('app.name') }}</a>.</strong> All
+            rights
             reserved.
         </footer>
     </div>
@@ -283,8 +262,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- AdminLTE App -->
     <script src="{{ asset('assets/') }}/dist/js/adminlte.min.js"></script>
 
-    <script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
-    <script src="{{ asset('assets/custom/js/particles.js') }}"></script>
     <script src="{{ asset('assets/custom/js/realtime.js') }}"></script>
 </body>
 
