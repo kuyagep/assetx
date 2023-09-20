3dcd6fecfe1d367e7fd22942ad229136177ccf5b
Geperson Mamalias <kuyagepofficial@gmail.com>
2023-09-20 09:31:20 +0800

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
      */
     public function store(Request $request)
     {
-        if(!empty($request->get('username'))){
+        if(!empty($request->get('user'))){
             return view('dashboard');
         }
 
