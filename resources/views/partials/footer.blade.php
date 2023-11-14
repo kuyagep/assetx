c3cfba1caf5151a219ccd167bb16357e47852721
Geperson Mamalias <kuyagepofficial@gmail.com>
2023-11-14 10:33:36 +0800

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,11 +1,7 @@
 <footer class="main-footer">
     <strong> &copy; {{ date('Y') }} <a href="javascript:void(0)"
             class="text-secondary">{{ config('app.name', 'Laravel') }}</a>.</strong>
-    Developed with ❤️ by <b title="Geperson Mamalias">PROJECT DAVAOSUR TEAM</b>.
     <div class="float-right d-none d-sm-inline-block">
-        <a href="javascript:void(0)" class="text-secondary  mr-4"><b>License</b></a>
-        <a href="javascript:void(0)" class="text-secondary  mr-4"><b>Help</b></a>
-        <a href="javascript:void(0)" class="text-secondary  mr-4"><b>Contact</b></a>
-        <a href="javascript:void(0)" class="text-secondary"><b>Terms &amp; Conditions</b></a>
+        <span class="text-secondary"><small>Version 1.01.02</sm></a>
     </div>
 </footer>
