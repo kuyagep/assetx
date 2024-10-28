95fd8692277c0902a8d9c9be5a88de20246353a7
Geperson Mamalias <kuyagepofficial@gmail.com>
2024-10-28 21:24:35 +0800

    Update realtime.js

--- public/assets/custom/js/realtime.js
+++ public/assets/custom/js/realtime.js
@@ -1,66 +1,70 @@
 function showTime() {
-  var now = new Date();
-  var h = now.getHours(); // 0 - 23
-  var m = now.getMinutes(); // 0 - 59
-  var s = now.getSeconds(); // 0 - 59
-  var day = now.getDay();
-  var date = now.getDate();
-  var month = now.getMonth();
-  var year = now.getFullYear();
-  var session = "AM";
+    var now = new Date();
+    var hours = now.getHours(); // 0 - 23
+    var minutes = now.getMinutes(); // 0 - 59
+    var seconds = now.getSeconds(); // 0 - 59
+    var day = now.getDay();
+    var date = now.getDate();
+    var month = now.getMonth();
+    var year = now.getFullYear();
 
-  if (h == 0) {
-    h = 12;
-  }
+    var session = hours < 12 ? "AM" : "PM";
+    // store day and month name in an array
+    var dayNames = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    var monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
 
-  if (h > 12) {
-    h = h - 12;
-    session = "PM";
-  }
-  // store day and month name in an array
-  var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  var monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  // format date and time
-  h = h < 10 ? "0" + h : h;
-  m = m < 10 ? "0" + m : m;
-  s = s < 10 ? "0" + s : s;
-  date = date < 10 ? "0" + date : date;
+    if (hours == 0) {
+        hours = 12;
+    }
 
-  // display date and time
-  var date =
-    monthNames[month] +
-    " " +
-    date +
-    ", " +
-    year +
-    " " +
-    dayNames[day] +
-    " " +
-    h +
-    ":" +
-    m +
-    ":" +
-    s +
-    " " +
-    session;
+    // format date and time
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    date = date < 10 ? "0" + date : date;
 
-  document.getElementById("current-date").innerText = date;
-  document.getElementById("current-date").textContent = date;
+    // display date and time
+    var currentTime =
+        monthNames[month] +
+        " " +
+        date +
+        ", " +
+        year +
+        " " +
+        dayNames[day] +
+        " " +
+        hours +
+        ":" +
+        minutes +
+        ":" +
+        seconds +
+        " " +
+        session;
 
-  setTimeout(showTime, 1000);
+    document.getElementById("current-date").innerText = currentTime;
+
+    setTimeout(showTime, 1000);
 }
 
 showTime();
